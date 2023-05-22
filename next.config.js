2d061b57631a1d68457725c0dac9abf314539539
Theo <ekunnusitheophilus@gmail.com>
2023-05-22 18:05:41 +0100

    fix: style flickering bug fix

--- next.config.js
+++ next.config.js
@@ -5,17 +5,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  presets: ['next/babel'],
-  plugins: [
-    [
-      'styled-components',
-      {
-        ssr: true,
-        displayName: true,
-        preprocess: false,
-      },
-    ],
-  ],
 };
 
 module.exports = nextConfig;

66c45967d7d0af1f9544db343dd16d1225520517
Theo <ekunnusitheophilus@gmail.com>
2023-06-14 13:03:24 +0100

    fix: variables for dev, prod and test enviroments specified.

--- src/app/services/kanbanApi.ts
+++ src/app/services/kanbanApi.ts
@@ -2,8 +2,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `https://thekanban.vercel.app/api`,
-  // baseUrl: `http://localhost:3000/api`,
+  baseUrl:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/api'
+      : process.env.NEXT_PUBLIC_API_URL,
 });
 
 export const kanbanApi = createApi({
